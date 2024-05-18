7526801e3ae0fb7cab5a83650eaa51a929db982d
levanbang376 <levanbang376@gmail.com>
2024-05-19 00:42:57 +0700

    update base URL API

--- services/HTTPServices.jsx
+++ services/HTTPServices.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'https://bank-debt-datn.vercel.app'
 
 const HTTPService = {
   sendRequest: (
